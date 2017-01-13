8b59cdd7894a7e9b5fd43fc1dc249b6df379cad0
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-13 22:28:50 +0000

    Refactored max-profit.js to use reduce instead of loops.

--- max-profit.js
+++ max-profit.js
@@ -1,29 +1,17 @@
 // Brute Force
 
 function maxProfit(inputArr) {
-  var maxProfit = 0;
-
-  for (var i = 0; i <= inputArr.length - 1; i++) {
-    maxProfit = checkRestOfArray(inputArr, i) > maxProfit ? checkRestOfArray(inputArr, i) : maxProfit;
-  }
-
-  if (maxProfit === 0) {
-    return -1;
-  }
-
-  return maxProfit;
+  return inputArr.reduce(function(a, element, index) {
+    var maxDiff = -1;
+    for (var i = index + 1; i <= inputArr.length; i++) {
+      var diff = inputArr[i] - element;
+      maxDiff = (diff > maxDiff) ? diff : maxDiff;
+    }
+    return Math.max(a, maxDiff);
+  }, -1);
 }
 
-function checkRestOfArray(inputArr, index) {
-  var startVal = inputArr[index];
-  var maxDiff = 0;
-
-  for (var i = index + 1; i <= inputArr.length; i++) {
-    var diff = inputArr[i] - startVal;
-    maxDiff = diff > maxDiff ? diff : maxDiff;
-  }
-  return maxDiff;
-}
+// Elegant solution?
 
 console.log(maxProfit([45, 24, 35, 31, 40, 38, 11]));
 console.log(maxProfit([1, 2, 3, 4, 55, 6, 2, 8, 9]));
